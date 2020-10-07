0bcb3d363d943f991e86ad7570dc660b3a701117
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:20:47 -0500

    Add files via upload

---
+++ RectangleRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+
+public class RectangleRunner
+{
+	public static void main( String[] args )
+	{
+		Rectangle test = new Rectangle();
+		test.setLengthWidth(2,6);
+		test.calculatePerimeter( );
+		test.print();
+
+		test.setLengthWidth(12,5);
+		test.calculatePerimeter( );
+		test.print();
+
+		//add more test cases
+		
+	}
+}
